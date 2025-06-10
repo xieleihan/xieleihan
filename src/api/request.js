974b866f8eb4672b16ef083fa0fb992b094baf7e
南秋SouthAki <57227318+xieleihan@users.noreply.github.com>
2025-06-10 10:26:21 +0800

    fix:修复请求

--- src/api/request.js
+++ src/api/request.js
@@ -13,7 +13,7 @@ const getInfo = async () => {
     console.warn('当前用户访问的IP地址信息:', ipinfo?.text);
     console.warn('当前用户位置信息:', adcode?.o);
 
-    return `当前用户访问的IP地址信息: ${ipinfo?.text}, 当前用户位置信息: ${adcode?.o}`;
+    return `当前Action的IP地址是: ${ipinfo?.text}, 位置在: ${adcode?.o}`;
 }
 
 module.exports = getInfo;
