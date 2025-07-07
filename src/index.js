ea18cc17cc62323449224b649b2ca1342c2f0861
南秋SouthAki <57227318+xieleihan@users.noreply.github.com>
2025-07-07 11:13:59 +0800

    feat:增加重试函数,防止action失败

--- src/index.js
+++ src/index.js
@@ -12,13 +12,27 @@ const {getInfo, getHitokoto} = require('./api/request');
 const tplPath = path.join(__dirname, './template.ejs');
 const outputPath = path.join(__dirname, '../README.md');
 
+async function retry(fn, maxAttempts = 3, delay = 10000) {
+    let lastError;
+    for (let i = 0; i < maxAttempts; i++) {
+        try {
+            return await fn();
+        } catch (error) {
+            console.error(`当前第 ${i + 1} 失败. 在 ${delay / 1000}s 后重试`);
+            lastError = error;
+            if (i < maxAttempts - 1) await new Promise(res => setTimeout(res, delay));
+        }
+    }
+    throw lastError;
+}
+
 const main = async () => {
     const updatedAt = dayjs().format('YYYY年MM月DD日 HH时mm分')
 
     const tplStr = fs.readFileSync(tplPath, 'utf8');
 
-    const ipinfo = await getInfo();
-    const hitokoto = await getHitokoto();
+    const ipinfo = await retry(() => getInfo(), 3, 10000);
+    const hitokoto = await retry(() => getHitokoto(), 3, 10000);
 
     const html = ejs.render(tplStr, {
         ...data,
